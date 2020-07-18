ca7d73e0346abac4e59828896dbdb6103c0da852
deepikakshirsagar <31217971+deepikakshirsagar@users.noreply.github.com>
2020-07-18 20:22:54 +0530

    Added description

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -9,6 +9,14 @@ export class HomeComponent implements OnInit {
   price = 133;
   duration = 3;
 
+  conceptList = ['Live seminars in the Virtual Classroom with Adobe Connect and separate telephone conference',
+  'Intensive format for learning intercultural skills and management/leadership skills',
+  'Led by experienced Berlitz teaching experts',
+  'Usually two 90-minute sessions plus preparatory, interim, and follow-up exercises',
+  'Interactive techniques like in face-to-face training',
+  'Available as open-enrollment courses or for company groups',
+  'Small groups of max. twelve participants']
+
   constructor() { }
 
   ngOnInit(): void {
